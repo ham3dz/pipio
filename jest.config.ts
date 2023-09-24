85c9ea69e1c0eaaf83dae906e8c4f322305bb49f
Danial Manavi <dmanavi@live.com>
2023-09-24 15:53:33 +0000

    test: setup test configuration

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'pipio',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage',
+};
---
+++ src/handlers.spec.ts
@@ -0,0 +1,9 @@
+import { lambdaWrapper } from './handlers';
+
+describe('handlers', () => {
+  describe('lambdaWrapper', () => {
+    it('should be ok', () => {
+      // lambdaWrapper
+    });
+  });
+});
