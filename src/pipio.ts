fd36e02843d903e36876178f7a490e0191e5b0f1
Danial Manavi <dmanavi@live.com>
2023-09-24 15:28:47 +0000

    chore: added comments and small refactoring

--- src/pipio.ts
+++ src/pipio.ts
@@ -1,53 +1,78 @@
-export type ChainHandler<TRequest, TResponse> = (
-  req: TRequest
-) => Promise<TResponse> | void;
+export type PipioHandler<T, U> = (req: T) => U | Promise<U> | void;
 
-export type BuildParams<TResponse> = {
-  onError?: (err: unknown) => any;
-  onSuccess?: (result: TResponse) => any;
+export type BuildParams = {
+  /**
+   * Error handler
+   * @param {Error|unknown} err Thrown error object
+   * @returns The pipeline output or throw error
+   */
+  onError?: (err: Error | unknown) => any;
 };
 
-export class Pipio<TResponse> {
+export class Pipio<U> {
   constructor(
-    private readonly stack: [
-      ...ChainHandler<any, any>[],
-      ChainHandler<any, TResponse>
+    protected readonly handlers: [
+      ...PipioHandler<any, any>[],
+      PipioHandler<any, U>
     ]
   ) {}
 
-  use<TNextResponse>(fn: ChainHandler<TResponse, TNextResponse>) {
-    return new Pipio<TNextResponse>([...this.stack, fn]);
+  /**
+   * Add a new middleware to the pipeline
+   * @param fn Middleware function
+   * @returns {Pipio} A new instance of the pipio with the handler setup
+   */
+  use<TNextResponse>(fn: PipioHandler<U, TNextResponse>) {
+    return new Pipio<TNextResponse>([...this.handlers, fn]);
   }
 
-  build(params?: BuildParams<TResponse>) {
+  /**
+   * Build the pipeline and create a callable function
+   * @param params Build-time parameters
+   * @returns The wrapper function of the pipeline
+   */
+  build(params?: BuildParams) {
     return async (...args: any[]) => {
-      let result: any = args;
+      let output: any = args;
 
-      for (const fn of this.stack) {
+      for (const fn of this.handlers) {
         try {
-          result = await fn(result);
+          // call the function with the previous call result
+          const handlerResult = fn(output);
+
+          // if handler returns a promise, the we need to wait,
+          // otherwise, we're good to go
+          if (handlerResult instanceof Promise) {
+            output = await handlerResult;
+          } else {
+            output = handlerResult;
+          }
         } catch (e) {
-          if (params?.onError) {
+          // user handles the error, pass the error to their handler
+          if (typeof params?.onError === "function") {
             return params?.onError(e);
           }
 
+          // no handler found, throw error
           throw e;
         }
       }
 
-      if (this.stack.length === 0) {
-        result = undefined;
+      // no more item in the stack
+      if (this.handlers.length === 0) {
+        output = undefined;
       }
 
-      if (params?.onSuccess) {
-        result = params.onSuccess(result);
-      }
-
-      return result;
+      return output;
     };
   }
 }
 
-export const pipio = <TResponse>(fn: ChainHandler<any[], TResponse>) => {
+/**
+ * pipio create factory
+ * @param fn Main middleware
+ * @returns {Pipio} A new instance of pipio with the first handler configured
+ */
+export const pipio = <U>(fn: PipioHandler<any[], U>): Pipio<U> => {
   return new Pipio([fn]);
 };
