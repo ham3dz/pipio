f1f773e67acdc75dfa15a664e60a785337294ec9
Danial Manavi <dmanavi@live.com>
2023-10-04 14:31:51 +0000

    feat: exported lambdaWrapper output type

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,5 +1,10 @@
 import { SyncPipioHandler } from './pipio';
 
+export type LambdaWrapperOutput<TEvent, TContext> = {
+  event: TEvent;
+  context: TContext;
+};
+
 /**
  * Lambda wrapper middleware
  * Convert lambda handler arguments to pipio compatible arguments
@@ -8,7 +13,7 @@ import { SyncPipioHandler } from './pipio';
 export const lambdaWrapper =
   <TEvent = any, TContext = any>(): SyncPipioHandler<
     Array<any>,
-    { event: TEvent; context: TContext }
+    LambdaWrapperOutput<TEvent, TContext>
   > =>
   (args: Array<any>) => ({
     event: args[0] as TEvent,
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './pipio';
 export * as handlers from './handlers';
+export { LambdaWrapperOutput } from './handlers';
 
 import { pipio } from './pipio';
 
